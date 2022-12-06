79413e13548a57dcc698b2228b73022c213fffc7
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2022-12-06 21:19:33 +0100

    adding normal js logic to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+
+  const day= props.date.toLocaleString('en-GB', {day: '2-digit'});
+  const month = props.date.toLocaleString('en-GB', {month: 'long'});
+  const year = props.date.toLocaleString('en-GB', {year: 'numeric'});
+
     return (
         <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
+        <div>
+          <div>{day}</div>
+          <div>{month}</div>
+          <div>{year}</div>
+        </div>
         <div className='expense-item__description'>
           <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
@@ -12,4 +21,4 @@ function ExpenseItem(props) {
     )
 }
 
-export default ExpenseItem 
+export default ExpenseItem
