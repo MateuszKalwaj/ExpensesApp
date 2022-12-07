997e61c86c01f172c5982bf8e007fd8c0c2482cd
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2022-12-07 13:08:49 +0100

    added ExpenseDate.js, reformat code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,17 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-  const day= props.date.toLocaleString('en-GB', {day: '2-digit'});
-  const month = props.date.toLocaleString('en-GB', {month: 'long'});
-  const year = props.date.toLocaleString('en-GB', {year: 'numeric'});
-
-    return (
-        <div className='expense-item'>
-        <div>
-          <div>{day}</div>
-          <div>{month}</div>
-          <div>{year}</div>
-        </div>
-        <div className='expense-item__description'>
-          <h2>{props.title}</h2>
-          <div className='expense-item__price'>${props.amount}</div>
-        </div>
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default ExpenseItem
